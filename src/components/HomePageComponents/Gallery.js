68d9f8377befb366d92af3cb07674039eef4500a
efrencav <efren@abstract-creative.com>
2019-11-16 21:35:56 -0600

    columns on images

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -64,5 +64,46 @@ export default function Gallery() {
 }
 
 const GalleryWrapper = styled.div`
+display:grid;
+grid-template-columns:auto;
+grid-row-gap:1rem;
+.item{
+    position:relative;
+
+}
+.info{
+    position:absolute;
+    top:0;
+    left:0;
+    background:${styles.colors.mainYellow};
+    padding: 0.1rem 0.3rem;
+    text-transform: capitalize;
+}
+@media(min-width:576px){
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1rem;
+
+}
+@media(min-width:768px){
+    grid-template-columns: repeat(3, 1fr);
+
+}
+@media(min-width:992px){
+    .gatsby-image-wrapper{
+        height:100%;
+    }
+    grid-template-areas: 
+    'one one two two'
+    'one one three three';
+    .item-1{
+    grid-area: one;
+    }
+    .item-2{
+        grid-area: two;
+    }
+    .item-3{
+        grid-area: three;
+    }
+}
 
 `
